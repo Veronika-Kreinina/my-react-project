048ea7bc759cc3bb2884ca0e5505e664b2a79b36
Veronika Kreinina <veronika.kr.0608@gmail.com>
2024-10-23 12:58:57 +0200

    start

---
+++ src/components/Product.jsx
@@ -0,0 +1,24 @@
+import './App.jsx'
+import PropTypes from 'prop-types'
+
+export default function Product({name,
+  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+  price,}) {
+
+  return (
+   <>
+      <h2>{name}</h2>
+	  <img src= {imgUrl} alt={name} width="640" />
+      <p>Price: {price} credits</p>
+    </>
+  );
+};
+
+
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string,
+  price: PropTypes.number.isRequired,
+};
+
